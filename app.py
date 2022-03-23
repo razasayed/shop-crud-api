ac042e010ff566cee7d62356e9e6ee9bafb9e207
Raza Sayed <raza.sayed@gmail.com>
2022-03-23 15:34:23 +0530

    Initialize SQLAlchemy

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from db import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:secret@db/shop-crud-api'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 api = Api(app)
+
+@app.before_first_request
+def create_table():
+    db.create_all()
 
 class HelloWorld(Resource):
     def get(self):
